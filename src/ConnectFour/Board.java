6ccfc085eefff046fa249fd6904611eb3926258e
Sammy Flemington <samflemington@gmail.com>
2022-04-18 20:35:07 -0600

    added stuff

--- src/ConnectFour/Board.java
+++ src/ConnectFour/Board.java
@@ -16,12 +16,23 @@ public class Board extends JPanel{
 	private Integer[][] board; // 2d array for storing pieces
 	private static int WIDTH = 7;
 	private static int HEIGHT = 6;
+	private int totalMoves = 0;
+	private int currentPlayer = 0;
 	
-	public Board() {
+	public Board(int currentPlayer) {
 		super();
 		board = new Integer[WIDTH][HEIGHT];
+		currentPlayer = currentPlayer;
+	}
+	public Board(Board b) {
+		super();
+		board = b.getBoard();
+		currentPlayer = b.getCurrentPlayer();
 	}
 	
+	public int getCurrentPlayer() {
+		return currentPlayer;
+	}
 	public void loadFile(String s) {
 		try {
 			File file = new File(s);
@@ -38,9 +49,14 @@ public class Board extends JPanel{
 				}
 				j++;
 			}
+			reader.close();
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
+	}
+	
+	public Integer[][] getBoard(){
+		return board;
 	}
 	
 	public boolean dropPiece(int piece, int col) {
@@ -53,14 +69,103 @@ public class Board extends JPanel{
 			}
 			// place piece
 			board[col][i] = piece;
+			totalMoves ++;
+			System.out.println(evaluate());
 			return true; // success
 		}
 		return false; // failed to place piece!
 	}
+	public boolean checkCanPlace(int col) {
+		if (board[col][0].equals(0)) {
+			return true;
+		}
+		return false;
+	}
+	public ArrayList<String> getDiagonalsDownRight() {
+		// Time for spaghetti code
+		ArrayList<String> data = new ArrayList<String>();
+		for (int i = -3; i < 3; i++) {
+			String temp = "";
+			for (int j = 0; j < 7; j++) { //longest possible diagonal is 6 long
+				int row = j;
+				int col = j + i;
+				if (row >= 0 && row < WIDTH && col >= 0 && col < HEIGHT) {
+					temp += board[row][col];
+				}
+			}
+			data.add(temp);
+		}
+		return data;
+	}
 	
+	public ArrayList<String> getDiagonalsDownLeft() {
+		// Time for spaghetti code
+		ArrayList<String> data = new ArrayList<String>();
+		for (int i = 3; i < 8; i++) {
+			String temp = "";
+			for (int j = 0; j < 6; j++) { //longest possible diagonal is 6 long
+				int row = j;
+				int col = -j + i;
+				if (row >= 0 && row < WIDTH && col >= 0 && col < HEIGHT) {
+					temp += board[row][col];
+				}
+			}
+			data.add(temp);
+		}
+		return data;
+	}
 	// Finds value of board for specified player (1 or 2)
-	public int evaluate(int piece) {
-		return -1;
+	public int evaluate() {
+		// https://stackoverflow.com/questions/20802509/java-for-loops-how-to-check-for-winner-in-connect-4-program
+		// organize data for easy checking with built-in string functions
+		ArrayList<String> data = new ArrayList<String>();
+		// First, columns
+		for (Integer[] col : board) {
+			String colString = "";
+			for (Integer row : col) {
+				colString += row.toString();
+			}
+			data.add(colString);
+		}
+		
+		// now rows
+		for (int i = 0; i < HEIGHT; i++) {
+			String rowString = "";
+			for (Integer[] col : board) {
+				rowString += col[i];
+			}
+			data.add(rowString);
+		}
+		
+
+		data.addAll(getDiagonalsDownRight());
+
+		data.addAll(getDiagonalsDownLeft());
+		
+		// Now check for four in a row!
+		int points = 0;
+		// Generate key string
+		String key = "";
+		for (int i = 0; i < 4; i++) {
+			key += Integer.toString(1);
+		}
+		for (String s : data) {
+			if (s.contains(key)) {
+				points ++;
+			}
+		}
+		
+		// now check opposing player (2)
+		key = "";
+		for (int i = 0; i < 4; i++) {
+			key += Integer.toString(2);
+		}
+		for (String s : data) {
+			if (s.contains(key)) {
+				points --;
+			}
+		}
+		return points;
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -90,6 +195,10 @@ public class Board extends JPanel{
 		}
 		
 		repaint();
+	}
+	
+	public int getTotalMoves() {
+		return totalMoves;
 	}
 	public int getNumCols() {
 		return WIDTH;
--- src/ConnectFour/ConnectFour.java
+++ src/ConnectFour/ConnectFour.java
@@ -12,14 +12,17 @@ import javax.swing.JOptionPane;
 public class ConnectFour extends JFrame{
 	static Board displayBoard;
 	private int turn = 1;
+	private MinMax mm;
 	public ConnectFour() {
 		super();
-		displayBoard = new Board();
+		displayBoard = new Board(1);
 		displayBoard.loadFile("data/input2.txt");
 		add(displayBoard, BorderLayout.CENTER);
 		addMouseListener(new GameListener());
+		mm = new MinMax(displayBoard, 1);
 	}
 	
+	// this is bad implemnetation bc what if piece placement fails
 	private int nextTurn() {
 		return ((turn += 1) % 2) + 1;
 	}
@@ -35,6 +38,8 @@ public class ConnectFour extends JFrame{
 			int col = Math.round(x / colWidth);
 			// try to place a piece here for player 
 			displayBoard.dropPiece(nextTurn(), col);
+			Node n = new Node(getBoard(), 1);
+			System.out.println(mm.minimax(n, 5, true));
 			
 			repaint();	// Repaint the panel
 		}
@@ -45,6 +50,11 @@ public class ConnectFour extends JFrame{
 		public void mouseExited(MouseEvent e) {}
 		
 	}
+	
+	public Board getBoard() {
+		return displayBoard;
+	}
+	
 	public static void main(String[] args) {
 		ConnectFour game = new ConnectFour();
 		
