63859de90b49362ff23efa609b6d22fa923e75f3
Sammy Flemington <samflemington@gmail.com>
2022-04-18 18:47:56 -0600

    Connect 4 game logic implemented

--- src/ConnectFour/Board.java
+++ src/ConnectFour/Board.java
@@ -1,14 +1,18 @@
 package ConnectFour;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.JPanel;
+
 /*
  * Handles storing board info, placing pieces, etc
  */
-public class Board {
+public class Board extends JPanel{
 	private Integer[][] board; // 2d array for storing pieces
 	private static int WIDTH = 7;
 	private static int HEIGHT = 6;
@@ -18,29 +22,33 @@ public class Board {
 		board = new Integer[WIDTH][HEIGHT];
 	}
 	
-	public void loadFile(String s) throws FileNotFoundException {
-		File file = new File(s);
-		Scanner reader = new Scanner(file);
-		int j = 5;
-		while (reader.hasNextLine()) {
-			String line = reader.nextLine();
-			if (line.length() < 5)
-				break;
-			
-			for (int i = 0; i < line.length(); i++) {
-				char c = line.charAt(i);
-				board[i][j] = Character.getNumericValue(c);
+	public void loadFile(String s) {
+		try {
+			File file = new File(s);
+			Scanner reader = new Scanner(file);
+			int j = 0;
+			while (reader.hasNextLine()) {
+				String line = reader.nextLine();
+				if (line.length() < 5)
+					break;
+				
+				for (int i = 0; i < line.length(); i++) {
+					char c = line.charAt(i);
+					board[i][j] = Character.getNumericValue(c);
+				}
+				j++;
 			}
-			j--;
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
 		}
 	}
 	
-	public boolean dropPiece(int col, int piece) {
+	public boolean dropPiece(int piece, int col) {
 		// check if there is room for a piece in this column
-		if (board[col][HEIGHT - 1].equals(0)){
+		if (board[col][0].equals(0)){
 			// OK to drop piece
 			int i = HEIGHT - 1;
-			while (board[col][i] == 0 && i > 0) {
+			while (board[col][i] != 0 && i > 0) {
 				i--;// move down column until there is a piece or we hit bottom
 			}
 			// place piece
@@ -53,5 +61,37 @@ public class Board {
 	// Finds value of board for specified player (1 or 2)
 	public int evaluate(int piece) {
 		return -1;
+	}
+	
+	public void paintComponent(Graphics g) {
+		// Must call super method to do the preliminary stuffs
+		super.paintComponent(g);
+
+		// Recalculate cell size in case the window gets resized
+		int cellWidth = Math.round(getWidth() / WIDTH);
+		int cellHeight = Math.round(getHeight() / HEIGHT);
+		g.setColor(Color.BLACK);
+		g.drawRect(0, 0, getWidth(), getHeight());
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				// draw cells in correct color
+				switch(board[i][j]) {
+					case 0: g.setColor(Color.WHITE); break;
+					case 1: g.setColor(Color.GREEN); break;
+					case 2: g.setColor(Color.RED); break;
+					default: g.setColor(Color.BLACK); break;
+				}
+				// Draw cell
+				g.fillOval(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
+				// black outline
+				g.setColor(Color.BLACK);
+				g.drawRect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
+			}
+		}
+		
+		repaint();
+	}
+	public int getNumCols() {
+		return WIDTH;
 	}
 }
--- src/ConnectFour/ConnectFour.java
+++ src/ConnectFour/ConnectFour.java
@@ -1,5 +1,55 @@
 package ConnectFour;
 
-public class ConnectFour {
+import java.awt.BorderLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+
+public class ConnectFour extends JFrame{
+	static Board displayBoard;
+	private int turn = 1;
+	public ConnectFour() {
+		super();
+		displayBoard = new Board();
+		displayBoard.loadFile("data/input2.txt");
+		add(displayBoard, BorderLayout.CENTER);
+		addMouseListener(new GameListener());
+	}
 	
+	private int nextTurn() {
+		return ((turn += 1) % 2) + 1;
+	}
+	
+	private class GameListener implements MouseListener {
+
+		public void mouseClicked(MouseEvent e) {			
+		// Press and release of the mouse button
+					
+			int x = e.getX();
+			int y = e.getY();
+			int colWidth = getWidth() / displayBoard.getNumCols();
+			int col = Math.round(x / colWidth);
+			// try to place a piece here for player 
+			displayBoard.dropPiece(nextTurn(), col);
+			
+			repaint();	// Repaint the panel
+		}
+
+		public void mousePressed(MouseEvent e) {}
+		public void mouseReleased(MouseEvent e) {}
+		public void mouseEntered(MouseEvent e) {}
+		public void mouseExited(MouseEvent e) {}
+		
+	}
+	public static void main(String[] args) {
+		ConnectFour game = new ConnectFour();
+		
+		game.setSize(720,720);
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+		game.setVisible(true);
+	}
 }
